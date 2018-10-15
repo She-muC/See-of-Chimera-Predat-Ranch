9f85cdac30591caa4d5a05b315250bf9c2607062
DESKTOP-SO4VDPR\kazum <ursbt1270@gmail.com>
2018-10-15 23:14:50 +0900

    キメラ選択画面＿漆畑

---
+++ UnityScenes/KimeraSentaku/Assets/Scripts/ChimeraNameScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChimeraNameScript : MonoBehaviour {
+
+    public Text chimera;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
