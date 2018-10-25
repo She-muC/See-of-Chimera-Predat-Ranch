fd9dda10aba6e4279c591ff9f9df8ba863525c9b
yuzuriha <m0805h0825@gmail.com>
2018-10-25 09:29:34 +0900

    データの統合作業２_ゆず

---
+++ See of Chimera Predat Ranch/Assets/Scripts/UserController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UserController : MonoBehaviour {
+
+    PlayerController m_playerController;
+
+    bool m_isAttacking;
+
+    float m_attackintervalTimer;
+
+    [SerializeField]
+    KeyCode m_forwardKey = KeyCode.UpArrow;
+    [SerializeField]
+    KeyCode m_BackKey = KeyCode.DownArrow;
+    [SerializeField]
+    KeyCode m_rearKey = KeyCode.RightArrow;
+    [SerializeField]
+    KeyCode m_frontKey = KeyCode.LeftArrow;
+    [SerializeField]
+    KeyCode m_upKey = KeyCode.W;
+    [SerializeField]
+    KeyCode m_downKey = KeyCode.S;
+    [SerializeField]
+    KeyCode m_attackKey = KeyCode.A;
+    [SerializeField]
+    KeyCode m_attack2Key = KeyCode.Z;
+
+
+
+    //上キーが押された
+    bool m_isRunning;
+
+    float m_time0fKeyUp;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        m_playerController = GetComponent<PlayerController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        KeyCode forwardKey = m_forwardKey;
+        KeyCode backKey = m_BackKey;
+      //  if(m_playerController.Side == PlayerController.Sidestate.Right)
+        //{
+          //  forwardKey = m_BackKey;
+            //backKey = m_forwardKey;
+        //}
+
+        //前後移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (!m_isRunning)
+            {
+                if(Time.time - m_time0fKeyUp < 0.3f)
+                {
+                    //前進キーが押されてから0.3秒後にもう一度押されたら走る
+                    m_isRunning = true; 
+                }
+            }
+
+            if (m_isRunning)
+            {
+                m_playerController.Forward = 5.0f; //走っているなら
+            }
+            else
+            {
+                m_playerController.Forward = 1.0f;  //歩いているなら
+            }
+
+            //m_playerController.Forward = 1.0f * 2f;　//アナログ値　*　移動量(m/s)←最大値
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            m_playerController.Back = 1.0f;
+            m_isRunning = false;
+        }
+        else
+        {
+            m_isRunning = false;
+        }
+
+        //上キーが話された
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            m_time0fKeyUp = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_playerController.Left = 2.0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            m_playerController.Right = 2.0f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_playerController.Up = 2.0f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_playerController.Down = 2.0f;
+        }
+
+
+
+        if (!m_isAttacking && m_attackintervalTimer <= 0)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                UnityAction action = AttackFinished;
+                m_playerController.PlayAnimation("Attack", action);
+                m_isAttacking = true;
+            }
+
+            else if (Input.GetKey(KeyCode.Z))
+            {
+                UnityAction action = AttackFinished;
+                m_playerController.PlayAnimation("Attack2", action);
+                m_isAttacking = true;
+            }
+        }
+        else
+        {
+            m_attackintervalTimer -= Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// 攻撃が完了したら呼ばれる
+    /// </summary>
+    public void AttackFinished()
+    {
+        m_attackintervalTimer = 0.2f;
+
+        m_isAttacking = false;
+
+ //       Debug.Log("AttackFinished");
+    }
+}
