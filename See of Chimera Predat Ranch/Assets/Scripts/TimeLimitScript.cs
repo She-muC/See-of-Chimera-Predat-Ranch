664c9a5c15f3b7f45861dd481c6aa7e1b554c24d
DESKTOP-SO4VDPR\kazum <ursbt1270@gmail.com>
2018-11-15 11:13:49 +0900

    制限時間２＿漆畑

---
+++ See of Chimera Predat Ranch/Assets/Scripts/TimeLimitScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeLimitScript : MonoBehaviour {
+
+    [SerializeField]
+    private int minute;
+
+    [SerializeField]
+    private float seconds;
+
+    private float oldSeconds;
+
+    private Text timerText;
+
+    [SerializeField]
+    Text fiveCount;
+
+    [SerializeField]
+    Text timeUp;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        minute = 0;
+
+        seconds = 10;
+
+        oldSeconds = 0;
+
+        fiveCount.enabled = false;
+
+        timeUp.enabled = false;
+
+        timerText = GetComponentInChildren<Text>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        seconds -= Time.deltaTime;
+
+        if (seconds <= 0)
+        {
+            minute--;
+            seconds = seconds + 60;
+        }
+
+        //値が変わった時だけテキストUIを更新
+        if ((int)seconds != (int)oldSeconds)
+        {
+            timerText.text = minute.ToString("TimeLimit 00") + ":" + ((int)seconds).ToString("00");
+        }
+        oldSeconds = seconds;
+
+        //残り5秒になったら残り時間を2秒表示
+        if (minute <= 0 && (int)seconds <= 5)
+        {
+            fiveCount.enabled = true;
+
+            if ((int)seconds <= 3)
+            {
+                fiveCount.enabled = false;
+
+            }
+
+        }
+
+        //タイムアップを表示後リザルト画面へ遷移
+        if (minute <= 0 && (int)seconds <= 0)
+        {
+            timeUp.enabled = true;
+
+            Invoke("SceneTransition", 3f);
+
+            timerText.enabled = false;
+
+        }
+    }
+
+    void SceneTransition()
+    {
+        SceneManager.LoadScene("Result");
+
+    }
+
+}
